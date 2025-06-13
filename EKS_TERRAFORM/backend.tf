5700e9be2a730a38f317b46df1d50b34391e9601
onkarsherkar <onkar.sherkar@gmail.com>
2025-06-13 23:37:46 +0530

    update region code

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "hotstar14062025" # Replace with your actual S3 bucket name
+    bucket = "hotstar-clone14062025" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
